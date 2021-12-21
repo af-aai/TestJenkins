2aa8ac953964b3588383c10688c848735589e556
elyar-asqar <87675804+elyar-asqar@users.noreply.github.com>
2021-12-21 09:25:47 +0100

    ok

--- src/test/java/TestJenkins/stepdefinitions/normal.java
+++ src/test/java/TestJenkins/stepdefinitions/normal.java
@@ -12,7 +12,7 @@ public class normal {
     public void user_go_to_jenkins_remote_activate_page() throws InterruptedException {
 
         Thread.sleep(5000);
-        Hooks.driver.get("http://localhost:8080/job/Test_generalapp/build?token=elyarabdullah");
+        Hooks.driver.get("https://www.google.com/");
 
     }
 }
